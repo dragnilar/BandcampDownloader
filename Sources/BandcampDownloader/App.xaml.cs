319784b0b529c51742168068d169f6a0833db699
Great Dead Cthulhu <dragnilar@gmail.com>
2018-11-08 03:12:48 -0500

    Added theme selector

--- Sources/BandcampDownloader/App.xaml.cs
+++ Sources/BandcampDownloader/App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using System.Linq;
+using System.Windows;
 using BandcampDownloader.ViewModels;
+using DevExpress.Xpf.Core;
 
 namespace BandcampDownloader
 {
@@ -10,10 +12,28 @@ namespace BandcampDownloader
     {
         protected override void OnStartup(StartupEventArgs e)
         {
-            //
+            RemoveUnusedThemes();
             var mainViewModel = new MainViewModel();
             Current.MainWindow = new MainWindow(mainViewModel);
             Current.MainWindow.Show();
+        }
+
+        private void RemoveUnusedThemes()
+        {
+            foreach (var theme in Theme.Themes.ToList())
+            {
+                switch (theme.Name)
+                {
+                    case Theme.Office2016BlackName:
+                    case Theme.VS2017LightName:
+                    case Theme.VS2017BlueName:
+                    case Theme.VS2017DarkName:
+                        continue;
+                    default:
+                        Theme.RemoveTheme(theme.Name);
+                        break;
+                }
+            }
         }
     }
 }
